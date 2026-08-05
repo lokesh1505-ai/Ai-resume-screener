1caddc95cfb4a86d14155b5da4e7c4d83001cd78
Lokesh <lokeshkspm@gmail.com>
2026-08-05 20:38:59 +0530

    Update screener.py

--- screener.py
+++ screener.py
@@ -1,7 +1,7 @@
 import re
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from utils.experience_parser import extract_years_experience
+from experience_parser import extract_years_experience
 
 
 def clean_text(text):
